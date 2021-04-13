9a75be9aa0023645d68b98625ecb298a1599370d
Michael Francisco <m.francisco9@gmail.com>
2021-04-13 13:00:46 -0500

    added genre IDs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,20 +3,20 @@ var key = '8426e25c492b7e1c228e5403fd1be062';
 var requestUrl = 'https://api.themoviedb.org/3/movie/550?api_key=8426e25c492b7e1c228e5403fd1be062'
 
 // Runtime Slider
-var slider = document.getElementById('runTime-slider');
-  noUiSlider.create(slider, {
-   start: [1960, 2021],
-   connect: true,
-   step: 1,
-   orientation: 'horizontal', // 'horizontal' or 'vertical'
-   range: {
-     'min': 1960,
-     'max': 2021
-   },
-   format: wNumb({
-     decimals: 0
-   })
-  });
+// var slider = document.getElementById('runTime-slider');
+//   noUiSlider.create(slider, {
+//    start: [1960, 2021],
+//    connect: true,
+//    step: 1,
+//    orientation: 'horizontal', // 'horizontal' or 'vertical'
+//    range: {
+//      'min': 1960,
+//      'max': 2021
+//    },
+//    format: wNumb({
+//      decimals: 0
+//    })
+//   });
        
 
 var tmdb = "https://api.themoviedb.org/3/movie/550?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2"
