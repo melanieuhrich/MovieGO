7d073b46f06863ee6627cbab8ce6085342de49b7
Michael Francisco <m.francisco9@gmail.com>
2021-04-14 17:57:39 -0500

    added genres

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,8 @@
 
 var key = '8426e25c492b7e1c228e5403fd1be062';
 var requestUrl = 'https://api.themoviedb.org/3/movie/550?api_key=8426e25c492b7e1c228e5403fd1be062'
-var tmdb = "https://api.themoviedb.org/3/movie/550?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2"
-var genre = "https://api.themoviedb.org/3/genre/movie/list?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2&language=en-US"
 var movie = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2&language=en-US"
-var search = "https://api.themoviedb.org/3/search/movie?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2&query=Home+Alone"
-var key = "cdeeab3b93b63acfe6a1d14f6ac420d2"
+var keyMF = "cdeeab3b93b63acfe6a1d14f6ac420d2";
 
 // Runtime Slider
 var slider = document.getElementById('runTime-slider');
@@ -41,53 +38,54 @@ noUiSlider.create(slider, {
 });
 slider.noUiSlider.on('update', function (values){
     yearSlider2ValueElement.innerHTML = values.join(' - ');
-})
+});
 
-
-fetch(search)
-    .then(function (response) {
-        return response.json();
-    })
-    .then (function (data) {
-        console.log(data);
-        // console.log(data);
-    for (var i = 0; i < data.length; i++){
-        console.log(data);
-        console.log(data[i].adult);
-        console.log(data[i].original_title);
+function getGenres(){
+    genreList = [];
+    for (i = 0; i < $("#checkBoxForm").children().length - 1; i++)
+    if ($(".checkboxinput").eq(i).is(":checked")) {
+        genreList.push($(".checkboxinput").eq(i).attr("id"));
     }
-    })
+    return genreList.toString();
+};
+
+var genres = getGenres();
+var runtimeLte = 200;
+var releaseLte = 2020;
+var releaseGte = 1900;
+var voteGte = 5;
+// var adult = true;
+
+// var  getlink = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=${adult}&with_genres=${genres}&with_runtime.lte=${runtimeLte}release_date.gte=${releaseGte}&primary_release_date.lte=${releaseLte}&vote_average.gte=${voteGte}&page=10`;
+// var  getlink = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=${adult}&with_genres=${genres}&vote_average.gte=${voteGte}&page=10`;
 
 
-    fetch(genre)
-    .then(function (response){
-        return response.json();
-    })
-    .then (function (data) {
-        console.log(data);
-    })
 
-
-
-
-
-
-var genres = 28;
-var runtimeLte = 100;
-var releaseLte = 1991;
-var releaseGte = 1980;
-var voteGte = 8;
-var adult = true;
-
-var  getlink = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=${adult}&with_genres=${genres}&with_runtime.lte=${runtimeLte}release_date.gte=${releaseGte}&primary_release_date.lte=${releaseLte}&vote_average.gte=${voteGte}&page=10`;
-
-fetch(getlink) 
+function getMovie() {
+    var adult = $(".checkboxinput").eq(19).is(":checked");
+    console.log(adult);
+    var genres = getGenres();
+    var  getlink = `https://api.themoviedb.org/3/discover/movie?api_key=${key}&language=en-US&sort_by=popularity.desc&include_adult=${adult}&with_genres=${genres}&vote_average.gte=${voteGte}&page=10`;
+    console.log(getlink);
+    console.log(genres);
+    fetch(getlink) 
     .then(function (yourpick){
         return yourpick.json();
     })
     .then (function (data) {
         console.log(data);
     });
+
+}
+
+// function getGenres(){
+//     genreList = [];
+//     for (i = 0; i < $("#checkBoxForm").children().length - 1; i++)
+//     if ($(".checkboxinput").eq(i).is(":checked")) {
+//         genreList.push($(".checkboxinput").eq(i).attr("id"));
+//     }
+//     return genreList.toString();
+// }
 
 
 var submitBtn = document.getElementById('submit-btn');
@@ -96,49 +94,72 @@ function suggestMovie() {
     console.log('working');
     var right = document.getElementById('right-panel')
     var recBox = document.createElement('div');
+    var titleBox = document.createElement('div');
     var movieBox = document.createElement('div');
     var trailerBox = document.createElement('div');
-    var basedOn = document.createElement('h3');
+    var basedOn = document.createElement('h5');
     var title = document.createElement('p');
     var genre = document.createElement('p');
     var year = document.createElement('p');
     var runTime = document.createElement('p');
     var familyFriendly = document.createElement('p');
     var rating = document.createElement('p');
-    basedOn.textContent = 'Based on your preferences, we suggest:';
+    var trailer = document.createElement('div');
+    basedOn.textContent = 'Based on your preferences, we suggest:'; 
+    title.textContent = 'Title: '; // come back
+    genre.textContent = 'Genre: '; // come back
+    year.textContent = 'Year: '; // come back
+    runTime.textContent = 'Run time: '; // come back
+    familyFriendly.textContent = 'Family-friendly: '; // come back
+    rating.textContent = 'Rating';  // come back
+    trailer.textContent = 'Trailer: '; // come back
     console.log('movie');
     right.appendChild(recBox);
-    recBox.appendChild(movieBox);
+    recBox.appendChild(basedOn);
+    recBox.appendChild(titleBox);
     recBox.appendChild(trailerBox);
-    movieBox.appendChild(basedOn);
+    recBox.appendChild(movieBox);
+    titleBox.appendChild(title);
+    trailerBox.appendChild(trailer);
+    movieBox.appendChild(title);
+    movieBox.appendChild(genre);
+    movieBox.appendChild(year);
+    movieBox.appendChild(runTime);
+    movieBox.appendChild(familyFriendly);
+    movieBox.appendChild(rating);
+
+
 
 }  
 
-submitBtn.addEventListener('click', suggestMovie)
+// submitBtn.addEventListener('click', suggestMovie);
 
     
 
 // checkbox boolean + genre id    
-($(".checkboxinput").eq(0).attr("id"));
-($(".checkboxinput").eq(0).is(":checked"));
+// ($(".checkboxinput").eq(0).attr("id"));
+// ($(".checkboxinput").eq(0).is(":checked"));
 
 
 //test button
 
 $(".btn").on("click", function () {
+    $("#right-panel").empty();
     var list = getGenres();
     console.log(list);
+    getMovie();
+    suggestMovie();
 }
 );
 
-function getGenres(){
-    genreList = [];
-    for (i = 0; i < $("#checkBoxForm").children().length - 1; i++)
-    if ($(".checkboxinput").eq(i).is(":checked")) {
-        genreList.push($(".checkboxinput").eq(i).attr("id"));
-    // console.log(genreList);
-    // console.log(genreList.toString());
-    }
-    return genreList.toString();
-}
+// function getGenres(){
+//     genreList = [];
+//     for (i = 0; i < $("#checkBoxForm").children().length - 1; i++)
+//     if ($(".checkboxinput").eq(i).is(":checked")) {
+//         genreList.push($(".checkboxinput").eq(i).attr("id"));
+//     // console.log(genreList);
+//     // console.log(genreList.toString());
+//     }
+//     return genreList.toString();
+// }
 
