3c5f3ad4ff668f0748bf6858a282efd18724ba19
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2021-04-15 10:05:57 -0500

    updated JS from gitHub

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,7 +4,6 @@ var requestUrl = 'https://api.themoviedb.org/3/movie/550?api_key=8426e25c492b7e1
 var tmdb = "https://api.themoviedb.org/3/movie/550?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2"
 var genre = "https://api.themoviedb.org/3/genre/movie/list?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2&language=en-US"
 var movie = "https://api.themoviedb.org/3/movie/{movie_id}?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2&language=en-US"
-var search = "https://api.themoviedb.org/3/search/movie?api_key=cdeeab3b93b63acfe6a1d14f6ac420d2&query=Home+Alone"
 var key = "cdeeab3b93b63acfe6a1d14f6ac420d2"
 
 // Runtime Slider
